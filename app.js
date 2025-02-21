fffd4009e8f29d13dac4b6dbf4fad0377578b862
gkourouma <gkourouma2@gmail.com>
2025-02-20 19:03:40 -0600

    Updated Lab with Level Up Questions

--- app.js
+++ app.js
@@ -369,4 +369,77 @@ console.log('Exercise 9 my result: ', idx);
 
 
 
+/* ***********************************************************************************************
+Level Up exercise 1: Array.prototype.reduce()
+
+Calculate the combined lifespan of all the inventors using 
+Array.prototype.reduce()
+
+- Each object in the array includes these properties: 
+  'first', 'last', 'year' (birth year), and 'passed' (year of death).
+- Use the Array.prototype.reduce() method to calculate the sum of the total 
+  years lived by all the inventors.
+- Store the total sum in the variable 'totalYearsLived'.
+
+Hints:
+
+- Inside the reduce callback function, calculate the lifespan of each inventor 
+  (passed - year).
+- Accumulate this lifespan in the 'totalYearsLived' variable.
+- Remember, reduce takes a callback function and an initial value for the 
+  accumulator.
+*/
+
+// Complete the exercise in the space below:
+let totalYearsLived = inventors.reduce(function(accumulator, inventor) {
+    let lifespan = inventor.passed - inventor.year
+    return accumulator + lifespan
+  }, 0);
+// Check your work:
+console.log('Level Up 1 my result: ', totalYearsLived);
+// console.log('Level Up 1 correct result: ', 861);
+
+
+/* ***********************************************************************************************
+Level Up exercise 2: Array.prototype.reduce()
+
+Tallying travel methods using Array.prototype.reduce(). 
+
+Count the number of times each travel method appears in the 'travelMethods'
+array.
+
+- The resulting object should have keys as the travel methods 
+  ('car', 'truck', 'bike', etc.) and values as their respective counts.
+- Store this object in the variable 'travelMethodCounts'.
+
+Hints:
+- Inside the reduce function, check if the travel method already exists as a key
+  in your accumulator object. If it does, increment its count. If not, add it 
+  to the object and give it a value of 1.
+- Since you want to return an object, be sure to pass an empty {} for the 
+  initial value of the "accumulator".
+*/
+
+
+// Complete the exercise in the space below:
+let travelMethodCounts = travelMethods.reduce((accumulator,method) => {
+    if (accumulator[method]) {
+        accumulator[method] = accumulator[method] + 1;
+    } else {
+        accumulator[method] = 1
+    }
+    return accumulator;
+}, {}) ;
+
+// Check your work:
+console.log('Level Up 2 my result: ', travelMethodCounts);
+// console.log('Level Up 2 correct result: ', {
+//   car: 5,
+//   truck: 3,
+//   bike: 2,
+//   walk: 2,
+//   van: 2,
+// });
+
+
   
